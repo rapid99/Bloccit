a03525586753af7ecbf26d0f9f2e2a0fbbfa695b
“Matt <matthyman03@gmail.com>
2016-12-30 18:29:58 -0700

    checkpoint-26

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   resources :advertisements
   resources :questions
   resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
+
   post 'users/confirm' => 'users#confirm'
 
   get 'about' => 'welcome#about'
