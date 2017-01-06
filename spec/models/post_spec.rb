cdf4a974c413f870164f88b996c37893e66f4838
“Matt <matthyman03@gmail.com>
2017-01-05 18:06:39 -0700

    assignment-30

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -71,4 +71,17 @@ RSpec.describe Post, type: :model do
        end
      end
   end
-end
+
+  describe "#create_vote"
+    it "sets the post up_votes to 1" do
+      expect(post.up_votes).to eq(1)
+    end
+    it "calls #create_vote when a post is created" do
+      post = topic.posts.new(title: RandomData.random_sentence, body: RandomData.random_sentence, user: user)
+      expect(post).to receive(:create_vote)
+      post.save
+    end
+    it "associates the vote with the owner" do
+      expect(post.votes.first.user).to eq(post.user)
+    end
+  end
