7209ea4dc4928c8b944d195ff90cf0183b9df03b
“Matt <matthyman03@gmail.com>
2017-01-06 17:44:54 -0700

    checkpoint-31

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,9 @@ module Bloccit
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    
+    # SENDGRID_USERNAME: app61085330@heroku.com
+    # SENDGRID_PASSWORD: xwtogoxv3252
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
